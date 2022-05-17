6c4af002ee1313c4cc96cdca9f9e6ffeb793bf26
Ryan <ryskim17@gmail.com>
2022-05-16 20:42:39 -0500

    base path

---
+++ docs/_next/static/nXJ_fBevKuumuu3d1xRF2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-00cc752315ec5e7b.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
