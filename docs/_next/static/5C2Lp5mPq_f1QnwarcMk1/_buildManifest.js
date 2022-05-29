2a98bb7159569321a45520cd4282ebbace4e6041
Ryan <ryskim17@gmail.com>
2022-05-29 05:59:55 -0500

    final update link

--- docs/_next/static/5C2Lp5mPq_f1QnwarcMk1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/981-27c1adad5064d8ee.js","static/chunks/pages/index-e7e1b5fba5d44703.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
